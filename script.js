bb46394e42ef6a8aabb9b087899a47230232b10b
pa2493 <code64901@gmail.com>
2025-08-05 20:48:38 +0530

    script.js

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ let profilePic = localStorage.getItem('profilePic') || '';
 let studyStartTime = null;
 let totalStudySeconds = parseInt(localStorage.getItem('studyTime')) || 0;
 
+ const firebaseConfig = {
+    apiKey: "AIzaSyAcOs3hyYea3BM55R5GB-F0hObDbxgNrqA",
+    authDomain: "study-web-8cd99.firebaseapp.com",
+    databaseURL: "https://study-web-8cd99-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "study-web-8cd99",
+    storageBucket: "study-web-8cd99.firebasestorage.app",
+    messagingSenderId: "320613093347",
+    appId: "1:320613093347:web:5bb57a0b5c83fdc0e09ad6",
+    measurementId: "G-TNZD8GNJFT"
+
+
 // Load existing data on startup
 window.onload = () => {
     document.getElementById('username').textContent = username;
@@ -121,4 +132,5 @@ function formatTime(seconds) {
     const mins = Math.floor(seconds / 60);
     const sec = seconds % 60;
     return `Total: ${mins}m ${sec}s`;
+
 }
